6c03be130f142fa62abdde9529b485924da8b721
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2023-09-11 22:26:27 -0500

    Se actualiza las IPs

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "35.175.214.146"]
+ALLOWED_HOSTS = ["localhost", "34.238.235.201"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.146.172.200",  # Dirección IP de la base de datos
+        "HOST": "18.208.178.103",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -157,7 +157,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.88.242.85"
+MQTT_HOST = "34.201.37.219"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
