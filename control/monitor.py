bf3acbb6f27c4656603027c066697f821708c1af
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2023-09-12 23:27:20 -0500

    Se corrige la desviación.

--- control/monitor.py
+++ control/monitor.py
@@ -56,7 +56,7 @@ def analyze_data():
             alerts += 1
 
     data2 = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(minutes=5))
+        base_time__gte=datetime.now() - timedelta(minutes=1))
     aggregation2 = data2.annotate(check_value=StdDev('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
@@ -82,7 +82,7 @@ def analyze_data():
         city = item['station__location__city__name']
         user = item['station__user__username']
 
-        if item["check_value"] > (max_value - min_value)/4 :
+        if item["check_value"] > (max_value - min_value) * 0.2 :
             alert = True
 
         if alert:
